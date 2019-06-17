0bc53a086dcbbdb801e031d4c9415f8b90c20d6e
imran khan <imrankhan4626@gmail.com>
2019-06-17 16:17:48 +0530

    fist commit

---
+++ khan.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#this in a comment
+echo "hello imran khan" #this is also a comment
+echo "imran khan and naheera banu" #this also comment
+echo $BASH
+echo $BASH VERSION
+echo $HOME
+echo $PWD
+
+echo $0 $1 $2 $3 '< ec
+ho $1 $2 $3'
+
+echo $1 $2 $3
+
+echo "imran khan  $Administrator"
+
+
+echo this is a $BASH
